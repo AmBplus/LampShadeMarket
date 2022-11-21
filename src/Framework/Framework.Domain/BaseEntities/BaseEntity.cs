8b607bef0c48a9ad7c33fb39b37301c83ce14e22
AMB <clasicnew1@gmail.com>
2022-11-21 20:04:08 +0330

    feat(create template) : Create Template & Also Create lib for ShopMan..

---
+++ src/Framework/Framework.Domain/BaseEntities/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using Utilities;
+
+namespace Framework.Domain.BaseEntities;
+
+public class BaseEntity
+{
+    public long Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime UpdateDate { get; set; }
+
+    public BaseEntity()
+    {
+        CreatedDate = DateUtility.DateTimeNow()
+    }
+}
---
+++ src/Framework/Framework.Shared/Utilities/DateUtility.cs
@@ -0,0 +1,13 @@
+﻿namespace Framwork.Utilities;
+
+public static class DateUtility
+{
+    public static DateTime DateTimeNow(this DateTime date)
+    {
+        return DateTime.Now;
+    }
+    public static DateTime DateTimeNow()
+    {
+        return DateTime.Now;
+    }
+}
